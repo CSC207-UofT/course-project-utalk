5edcf9d4296b676dfd2bb9d068090de45abcf96e
JiangKuang <90354197+JiangKuang@users.noreply.github.com>
2021-10-16 12:26:42 -0400

    Add files via upload

---
+++ course-project-utalk/src/main/LogController.java
@@ -0,0 +1,10 @@
+/**
+ * Controls the login process. If it's a new user, LogController will automatically create a new one. Otherwise, it will
+ * check the username and password
+ */
+
+
+public class LogController {
+
+
+}
