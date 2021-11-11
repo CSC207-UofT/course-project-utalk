5a8d0ee60c01d6bcb2460fad319a1f79fb553087
FrankQQQQ <18518553618@163.com>
2021-11-11 00:12:45 -0500

    You can run commandUI_new now.

--- src/main/basics/commandUI_new.java
+++ src/main/basics/commandUI_new.java
@@ -1,13 +1,13 @@
 package basics;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class commandUI_new {
     public static void main(String[] args) {
         //This is the header of the program.
-        System.out.println("\n================================================================\nWelcome to U talk!\n\n" +
-                "================================================================");
+        System.out.println("\n================================================================================================================================\nWelcome to U talk!\n\n");
 
         //put functions below.
         register_signin_ui();
@@ -19,15 +19,19 @@ public class commandUI_new {
 
 
 
-    //developer password is Qiao123456
+    //developer password is qiao123
     public static void register_signin_ui(){
         //this is command UI function for register and sign in.
+        System.out.println("================================================================================================================================");
         System.out.println("What would you like to do next? \n 1: register \n 2: login \n 9: quit program. \n10: administrator privileges");
         Scanner scan = new Scanner(System.in);
         String enter = scan.nextLine();
         switch (enter) {
-            case "1" -> System.out.println("Please follow the orders to register (wait for me debugging this function)");
-            case "2" -> System.out.println("PLease follow the orders to log in (wait for me debugging this function)");
+            case "1" -> {System.out.println("Please follow the orders to register");
+                register_new.register_ui();
+
+            }
+            case "2" -> {System.out.println("PLease follow the orders to log in (wait for me debugging this function)");}
             case "9" -> {
                 System.out.println("Are you sure you wish to quit? PLease type in 'yes' to quit.");
                 Scanner c = new Scanner(System.in);
@@ -46,7 +50,7 @@ public class commandUI_new {
                 System.out.println("Please enter developer password.");
                 Scanner a = new Scanner(System.in);
                 String password = a.nextLine();
-                if ("Qiao123456".equals(password)) {
+                if ("qiao123".equals(password)) {
                     //The line below eliminates infinite loop warning. Please don't remove it.
                     //noinspection InfiniteLoopStatement
                     while(true) {
@@ -59,17 +63,39 @@ public class commandUI_new {
                         String number = b.nextLine();
                         switch (number) {
                             case "1" -> {
-                                System.out.println("already checked all users");
+                                System.out.println("[id, username, password, type, status]");
+                                for(ArrayList<String> item : register_new.csv_to_list()){
+                                    System.out.println(item);
+                                }
+                                System.out.println("\n================================================================================================================================\n");
+                                System.out.println("What would you like to do next?");
                             }
                             case "2" ->{
-                                System.out.println("No! Don't do this! (This function is not written yet.)");
+                                System.out.println("Be careful! \nAre you sure you wish to delete the whole database? yes/no");
+                                Scanner scc = new Scanner(System.in);
+                                String result = scc.nextLine();
+                                if(result.equals("yes")){
+                                    delete_user_new.delete_all_user();
+                                    System.out.println("As you wish.");
+                                    System.out.println("Database deleted.");
+                                    System.out.println("\n================================================================================================================================");
+                                    System.out.println("What would you like to do next?");
+                                }
+                                else{
+                                    System.out.println("You did not delete the database");
+                                    System.out.println("\n================================================================================================================================");
+                                    System.out.println("What would you like to do next?");
+                                }
                             }
                             case "10" -> {
-                                System.out.println("\n================================================================");
+                                System.out.println("\n================================================================================================================================");
                                 register_signin_ui();
+
                             }
                             default -> {
-                                System.out.println("This is not a valid number. please type in again.");
+                                System.out.println("This is not a valid input. please type in again.");
+                                System.out.println("================================================================================================================================");
+
                             }
                         }
                     }
