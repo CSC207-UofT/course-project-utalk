929d6ba714e64bbe169d316503f7fc4786206bfa
ZhijunWang1 <90380043+ZhijunWang1@users.noreply.github.com>
2021-11-12 09:16:14 -0600

    Add files via upload

---
+++ CourseInfoPresenter.java
@@ -0,0 +1,9 @@
+package AddCourse;
+import AllCourses.coursePageHashMap;
+
+public class CourseInfoPresenter implements Presenter{
+    @Override
+    public String present_info(Object O) {
+        return null;
+    }
+}
---
+++ PagePresenter.java
@@ -0,0 +1,9 @@
+package AddCourse;
+
+public class PagePresenter implements Presenter{
+
+    @Override
+    public String present_info(Object O) {
+
+    }
+}
