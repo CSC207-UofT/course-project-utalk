bc8a68f73f052b9a1af74ef07ee16b3ada1aa7bd
ZhijunWang1 <90380043+ZhijunWang1@users.noreply.github.com>
2021-11-12 15:26:31 -0500

    Add files via upload

--- src/main/AddCourse/PostPage.java
+++ src/main/AddCourse/PostPage.java
@@ -6,16 +6,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class PostPage {
-    public Map<Integer, Comment> comments ;
+    public Map<Integer, Comment> comments;
+    public ArrayList<Comment> posts;
+
 
     String semester;
     public int current_id;
 
     PostPage(String semester_1) {
         this.comments = new HashMap<>();
+        posts = new ArrayList<>();
         semester = semester_1;
         current_id = 0;
 
+    }
+
+    void Print_comments() {
+        for (Comment comment : posts) {
+            comment.Print_Comment(0);
+        }
     }
 }
