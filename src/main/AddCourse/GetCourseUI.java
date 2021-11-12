8ef6e060ef285758a3ef1e9271949353fd41f3a8
Hanqi-Angelina <angelinaezery@gmail.com>
2021-11-12 15:54:17 -0500

    add while loop and add Test Case

--- src/main/AddCourse/GetCourseUI.java
+++ src/main/AddCourse/GetCourseUI.java
@@ -21,24 +21,22 @@ public class GetCourseUI {
         String year = input_help.getinput(System.in).nextLine();
         return new String[]{code, info, year};
     }
-    public static void add_course(){
-        String[] args = GetCourseUI.loaddata();
-        CreateandUpdate createandUpdate = new CreateandUpdate();
-        createandUpdate.runaddcourse(args);
-    }
-
-    public static void update_course(){
-        String[] args = GetCourseUI.loaddata();
-        CreateandUpdate createandUpdate = new CreateandUpdate();
-        createandUpdate.updatecourseinfo(args);
-    }
     public static void get_method(){
-        System.out.println("Choose which operation you want to make: \n 1: add course 2: update course \n");
+        System.out.println("""
+                Choose which operation you want to make:\s
+                 1: add course 2: update course\s
+                 Please enter 1 or 2""");
         String input_str = input_help.getinput(System.in).nextLine();
+        while (!(input_str.equals("1")||input_str.equals("2"))){
+            System.out.println("Please enter 1 or 2");
+            input_str = input_help.getinput(System.in).nextLine();
+        }
+        CreateandUpdate createandUpdate = new CreateandUpdate();
+        String[] args = GetCourseUI.loaddata();
         if (Objects.equals(input_str, "1")){
-            add_course();
+            createandUpdate.runaddcourse(args);
         } else{
-            update_course();
+            createandUpdate.updatecourseinfo(args);
         }
     }
 }
