5aa0ee3fde43b6e669c34704d39aa254eb81dafa
ZhijunWang1 <90380043+ZhijunWang1@users.noreply.github.com>
2021-11-14 16:37:40 -0500

    Add files via upload
    change on Friday's night

--- src/main/AddCourse/CreateCourse.java
+++ src/main/AddCourse/CreateCourse.java
@@ -7,12 +7,8 @@ import basics.Course;
 public class CreateCourse {
     public void create_course(String course_name, String description, String year)
     {
-        if (AllCourses.coursePageHashMap.containsKey(course_name)){
-            System.out.println("Course Already Exist, please do update course");
-        } else{
         Course new_course = new Course(course_name, description, year);
         CoursePage course_page = new CoursePage(new_course);
         AllCourses.coursePageHashMap.put(course_name, course_page);
-        System.out.println("Course added Successfully");}
     }
 }
--- src/main/AddCourse/GetInputIn.java
+++ src/main/AddCourse/GetInputIn.java
@@ -9,5 +9,6 @@ public class GetInputIn {
     /* this method is used to help turn the inputstream into Scanner type to help with the string construction. */
     public Scanner getinput(InputStream stream){
         return new Scanner(System.in);
+
     }
 }
