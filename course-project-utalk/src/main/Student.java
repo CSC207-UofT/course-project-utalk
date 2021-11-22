2c5c1e983015d2553208c7fae9d4e27a0c1b1180
Hanqi-Angelina <58234776+Hanqi-Angelina@users.noreply.github.com>
2021-11-22 10:57:52 -0500

    Delete course-project-utalk directory
    Want to use the revised version with proper packaging

--- course-project-utalk/src/main/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student extends User {
-    int student_number;
-    String student_name;
-    Student(int student_number, String student_name){
-        this.student_name = student_name;
-        this.student_number = student_number;
-    }
-    void add_course(String course){}
-}
