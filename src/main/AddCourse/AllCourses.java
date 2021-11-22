48882154ccd564037308a13efe82b4b69e8a001f
Hanqi-Angelina <58234776+Hanqi-Angelina@users.noreply.github.com>
2021-11-22 10:58:49 -0500

    Delete src directory
    delete the old version

--- src/main/AddCourse/AllCourses.java
+++
@@ -1,9 +0,0 @@
-package AddCourse;
-
-import java.util.Map;
-import java.util.HashMap;
-
-public class AllCourses {
-    public static Map<String, CoursePage> coursePageHashMap = new HashMap<>();
-
-}
--- src/main/basics/Add_Comment.java
+++
@@ -1,4 +0,0 @@
-package basics;
-public interface Add_Comment {
-    String add_comment(String content, String Course_code);
-}
