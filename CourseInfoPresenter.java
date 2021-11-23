02ce3deacd2ff27efed380d88bd9e60de74666a2
Kuang Jiang <kuang.jiang@outlook.com>
2021-11-23 13:22:08 -0500

    Delet all the presenter at the first layer

--- CourseInfoPresenter.java
+++
@@ -1,9 +0,0 @@
-package AddCourse;
-import AllCourses.coursePageHashMap;
-
-public class CourseInfoPresenter implements Presenter{
-    @Override
-    public String present_info(Object O) {
-        return null;
-    }
-}
