217de5a989307fb81df10fd80b1fd2266a69ae50
Kuang Jiang <kuang.jiang@outlook.com>
2021-11-24 20:32:17 -0500

    fix several little bugs and add getter,setter in comment course Professor and Student

--- src/main/entity/Comment.java
+++ src/main/entity/Comment.java
@@ -1,5 +1,7 @@
 package entity;
 
 public class Comment {
+    // in our previous implementation comment is recursively implemented, I suggest to use tree data structure to
+    // implement, maybe AVL tree is better, but it's still a binary tree data structure.
 
 }
--- src/main/entity/Student.java
+++ src/main/entity/Student.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Student extends User{
-    private ArrayList<String> student_course;
+    private ArrayList<String> studentCourse;
     private String password;
-    private HashMap<String, ArrayList<Comment>> student_comments = new HashMap<>();
+    private HashMap<String, ArrayList<Comment>> studentComments = new HashMap<>();
 
     public Student(String identifier, String user_name, String password) {
         super(identifier, user_name, password);
     }
 
-    public HashMap<String, ArrayList<Comment>> getStudent_comments() {
-        return student_comments;
+    public HashMap<String, ArrayList<Comment>> getStudentComments() {
+        return studentComments;
     }
 
-    public void setStudent_comments(HashMap<String, ArrayList<Comment>> student_comments) {
-        this.student_comments = student_comments;
+    public void setStudentComments(HashMap<String, ArrayList<Comment>> student_comments) {
+        this.studentComments = student_comments;
     }
 
 }
