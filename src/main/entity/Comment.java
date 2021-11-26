382d54a4accad65a278bbdf38f3905b726452a12
Kuang Jiang <kuang.jiang@outlook.com>
2021-11-26 17:51:21 -0500

    Add Enroll and Drop course methods to the usecase

--- src/main/entity/Comment.java
+++ src/main/entity/Comment.java
@@ -1,6 +1,16 @@
 package entity;
 
+import java.util.HashSet;
+
 public class Comment {
+    public HashSet<Comment> replies;
+    public String user_name;
+    public boolean status;
+    public String content;
+    public Integer id;
+
+    public void Print_Comment(int i) {
+    }
     // in our previous implementation comment is recursively implemented, I suggest to use tree data structure to
     // implement, maybe AVL tree is better, but it's still a binary tree data structure.
 
