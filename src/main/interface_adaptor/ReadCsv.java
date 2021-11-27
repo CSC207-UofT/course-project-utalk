0770d7c5d9dd9a20466b9cf41637b404f72c31f9
FrankQQQQ <86399894+FrankQQQQ@users.noreply.github.com>
2021-11-27 09:44:34 -0500

    Update ReadCsv.java

--- src/main/interface_adaptor/ReadCsv.java
+++ src/main/interface_adaptor/ReadCsv.java
@@ -1,9 +1,38 @@
 package interface_adaptor;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ReadCsv {
+    public static ArrayList<String> str_to_arraylist(String string, String split_by) {
+        //This is helper method.
+        //This function transfer from str to arraylist.
+        ArrayList<String> result = new ArrayList<>();
+        for (String item : string.split(split_by)) {
+            result.add(item);
+        }
+        return result;
+    }
 
+    public static ArrayList<ArrayList<String>> csv_to_list(String file_path) {
+        //Transfer a csv file into readable List<List<String>>. Each sublist is a row from the csv file.
+        String line = "";
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file_path));
+            while ((line = br.readLine()) != null) {
+                result.add(str_to_arraylist(line, ","));
+                //result.add(Collections.singletonList(line));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
