b573b7872fa270bb26064129fa7f8c787a1c63e5
ZeyuanLi0702 <90986918+ZeyuanLi0702@users.noreply.github.com>
2021-12-01 17:37:51 -0500

    Update AllReader.java

--- src/main/interfaceadaptor/readers/AllReader.java
+++ src/main/interfaceadaptor/readers/AllReader.java
@@ -28,6 +28,57 @@ public class AllReader {
         CourseReader.readCourse();
         StudentCreator.createStudent(students);
         ProfessorCreator.createProfessor(professors);
+        
+        HashMap<String, ArrayList<String>>  read = new HashMap<>();
+        for (ArrayList<String> comment: comments){
+
+            String courseCode = comment.get(0);
+            String semester = comment.get(1);
+            Integer ID = Integer.valueOf(comment.get(2));
+            String author = comment.get(3);
+            String content = comment.get(4);
+            Integer replyID = Integer.valueOf(comment.get(5));
+            boolean status = comment.get(6).equals("true");
+            Integer replyTo = Integer.valueOf(comment.get(7));
+            CoursePage coursepage = AllCourses.coursePageHashMap.get(courseCode);
+            if(!read.containsKey(courseCode)){
+                ArrayList<String> initial = new ArrayList<>();
+                read.put(courseCode, initial);
+            }else{
+                if(! read.get(courseCode).contains(semester)){
+                    PostPage postPage = new PostPage(semester);
+                    coursepage.post_page_List.add(postPage);
+                }
+            }
+            Comment newComment = new Comment(author, content, courseCode, replyTo, ID);
+            newComment.setReplyID(replyID);
+            newComment.setStatus(status);
+            HashMap<String, PostPage> map = coursepage.postPageHashMap();
+            map.get(semester).getComments().put(ID, newComment);
+            if (AllProfessors.ProfessorHashMap.containsKey(author)){
+                AllProfessors.ProfessorHashMap.get(author).addCommentToList(courseCode, newComment);
+            }
+            if (AllStudents.StudentHashMap.containsKey(author)){
+                AllStudents.StudentHashMap.get(author).addCommentToList(courseCode, newComment);
+            }
+
+        }
+        for (String courseCode: read.keySet()){
+            CoursePage coursepage = AllCourses.coursePageHashMap.get(courseCode);
+            for(PostPage postPage: coursepage.getPost_page_List()){
+                for(Comment comment : postPage.getComments().values()){
+                    if (comment.getReplyTo().equals(0)){
+                        postPage.posts.add(comment);
+                    }
+                    else {
+                        Integer a = comment.getReplyTo();
+                        Comment commentToReply = postPage.getComments().get(a);
+                        commentToReply.addReply(comment);
+                    }
+
+                }
+            }
+        }
 
     }
 }
