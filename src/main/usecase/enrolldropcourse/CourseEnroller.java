5f99182a7565d09b630c66f7d434c26139156b02
ZeyuanLi0702 <90986918+ZeyuanLi0702@users.noreply.github.com>
2021-12-01 18:05:52 -0500

    Update CourseEnroller.java

--- src/main/usecase/enrolldropcourse/CourseEnroller.java
+++ src/main/usecase/enrolldropcourse/CourseEnroller.java
@@ -1,42 +1,35 @@
 package usecase.enrolldropcourse;
 
+import entity.CommentableUser;
 import usecase.javastorage.AllCourses;
-import entity.Professor;
-import entity.Student;
+import entity.Comment;
+
+import java.util.ArrayList;
 
 public class CourseEnroller {
 
-    /**
-     * Add the course to your course list, If it does not exist it will return warning
-     *
-     * @param course_name the course you want to add to your course list
-     * @param professor the name of the current user
-     */
-    public static void teachingCourse(String course_name, Professor professor)
-    {
 
-        if (AllCourses.coursePageHashMap.containsKey(course_name)){
-            professor.getTaughtCourses().add(course_name);
-            System.out.println("Dear" + professor.user_name+ "Course:" + course_name + "added successfully");
-        } else{
-            System.out.println("Dear" + professor.user_name+ "Course:" + course_name + "did not Exist, please check the course code");
-        }
-    }
 
     /**
      * Add the course to your course list, If it does not exist it will return warning
      *
      * @param course_name the course you want to add to your course list
-     * @param student the name of the current user
+     * @param user the name of the current user
      */
-    public static void enrollCourse(String course_name, Student student)
+    public static void enrollCourse(String course_name, CommentableUser user)
     {
 
         if (AllCourses.coursePageHashMap.containsKey(course_name)){
-            student.getStudentCourses().add(course_name);
-            System.out.println("Dear " + student.user_name+ "Course: " + course_name + " added successfully");
+            if(! user.getCourseList().contains(course_name)){
+            user.getCourseList().add(course_name);
+            ArrayList<Comment> empty = new ArrayList<>();
+            user.getComments().put(course_name, empty);
+            System.out.println("Dear " + user.user_name+ "Course: " + course_name + " added successfully");}
+            else{
+                System.out.println("Dear " + user.user_name+ "You have already enrolled Course: " + course_name );
+            }
         } else{
-            System.out.println("Dear " + student.user_name+ "Course: " + course_name + " did not Exist, please check the course code");
+            System.out.println("Dear " + user.user_name+ "Course: " + course_name + " did not Exist, please check the course code");
         }
     }
 
