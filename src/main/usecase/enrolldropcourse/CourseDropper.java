430872e6a0b5993f6fd622fc1cc2270c5d776068
ZeyuanLi0702 <90986918+ZeyuanLi0702@users.noreply.github.com>
2021-12-01 17:39:32 -0500

    Update CourseDropper.java

--- src/main/usecase/enrolldropcourse/CourseDropper.java
+++ src/main/usecase/enrolldropcourse/CourseDropper.java
@@ -1,5 +1,6 @@
 package usecase.enrolldropcourse;
 
+import entity.CommentableUser;
 import entity.Professor;
 import entity.Student;
 
@@ -10,38 +11,22 @@ public class CourseDropper {
      * Remove the course from your course list, If it does not exist it will return warning
      *
      * @param course_name the course you want to remove from your course list
-     * @param professor the name of the current user
+     * @param user the name of the current user
      */
-    public static void dropCourse(String course_name, Professor professor)
+    public static void dropCourse(String course_name, CommentableUser user)
     {
 
-        if (professor.getTaughtCourses().contains(course_name)){
-            professor.getTaughtCourses().remove(course_name);
-            System.out.println("Dear" + professor.user_name+ "Course:" + course_name + "removed successfully");
+        if (user.getCourseList().contains(course_name)){
+            user.getCourseList().remove(course_name);
+            System.out.println("Dear" + user.user_name+ "Course:" + course_name + "removed successfully");
         } else{
-            System.out.println("Dear" + professor.user_name+ "Course:" + course_name + "did not Exist in your course " +
+            System.out.println("Dear" + user.user_name+ "Course:" + course_name + "did not Exist in your course " +
                     "list, please check the course code");
         }
     }
 
 
-    /**
-     * Remove the course from your course list, If it does not exist it will return warning
-     *
-     * @param course_name the course you want to remove from your course list
-     * @param student the name of the current user
-     */
-    public static void dropCourse(String course_name, Student student)
-    {
 
-        if (student.getStudentCourses().contains(course_name)){
-            student.getStudentCourses().remove(course_name);
-            System.out.println("Dear" + student.user_name+ "Course:" + course_name + "removed successfully");
-        } else{
-            System.out.println("Dear" + student.user_name+ "Course:" + course_name + "did not Exist in your course " +
-                    "list ,please check the course code");
-        }
-    }
 }
 
 
